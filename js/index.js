41b1aeb82f5c597d20c14a833feafc1c1a44ce8f
Amaro Silva <arcamos.j@gmail.com>
2019-10-07 11:38:28 -0300

    ajustes conteudo

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
 
-<<<<<<< HEAD
-function baidFunction() {
-    
-    $(".button-collapse").sideNav();
-    
-}
 
-
-
-baidFunction();
-
-=======
-
-$(".button-collapse").sideNav();
->>>>>>> e1a2821b4927478ebabad2ad12e422d7bc9cf6c6
+$("nav i").sideNav();
